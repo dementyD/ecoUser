ed2eec14cf367f4375e1fe769e7c22aac973e2d4
dementyD <118716880+dementyD@users.noreply.github.com>
2023-03-21 18:20:25 +0300

    Add files via upload

--- HumanFilter.java
+++ HumanFilter.java
@@ -3,8 +3,15 @@ package greenUsers;
 public class HumanFilter {
 
     public Human[] humanFilter(Human[] arrayHuman, int maxConsumption) {
-        Human[] resultHuman = new Human[arrayHuman.length];
-        for (int i = 0; i < arrayHuman.length; i++) {
+        int countGreenHuman = 0;
+        for (Human human : arrayHuman) {
+            if (human.getWaterCount() < maxConsumption && human.getGasCount1() + human.getGasCount2() < maxConsumption
+                    && human.getElectricCount1() + human.getElectricCount2() < maxConsumption) {
+                countGreenHuman++;
+            }
+        }
+        Human[] resultHuman = new Human[countGreenHuman];
+        for (int i = 0; i < resultHuman.length; i++) {
             if (arrayHuman[i].getWaterCount() < maxConsumption
                     && arrayHuman[i].getGasCount1() + arrayHuman[i].getGasCount2() < maxConsumption
                     && arrayHuman[i].getElectricCount1() + arrayHuman[i].getElectricCount2() < maxConsumption) {
@@ -14,4 +21,5 @@ public class HumanFilter {
         return resultHuman;
     }
 }
+
 
