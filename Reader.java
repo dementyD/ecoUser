795092e8c3cc14261c0c3a5c85855dbc34296c2b
dementyD <118716880+dementyD@users.noreply.github.com>
2023-03-21 19:05:37 +0300

    Add files via upload

--- Reader.java
+++ Reader.java
@@ -8,26 +8,25 @@ import java.util.Scanner;
 public class Reader {
 
     public Human[] readFromFile(Path path) {
-        long count = 0;
+        long countLines = 0;
+        int countHuman = 0;
+        Scanner sc = null;
         try {
-            count = Files.lines(path).count();
+            countLines = Files.lines(path).count();
+            sc = new Scanner(path);
         } catch (IOException e) {
             System.out.println("файл не найден");
         }
-        Human[] humans = new Human[(int) (count - 1)];
-        try {
-            Scanner sc = new Scanner(path);
-            Human.setHeadings(sc.nextLine());
-            int countHuman = 0;
-            while (sc.hasNext()) {
-                sc.useDelimiter("\\||\r\n");
-                Human human = new Human(sc.nextInt(), sc.next(), sc.nextInt(), sc.nextInt(),
-                        sc.nextInt(), sc.nextInt(), sc.nextInt());
-                humans[countHuman++] = human;
-            }
-        } catch (IOException e) {
-            System.out.println("файл не найден");
+        Human.setHeadings(sc.nextLine());
+
+        Human[] humans = new Human[(int) (countLines - 1)];
+        while (sc.hasNext()) {
+            sc.useDelimiter("\\||\r\n");
+            Human human = new Human(sc.nextInt(), sc.next(), sc.nextInt(), sc.nextInt(),
+                    sc.nextInt(), sc.nextInt(), sc.nextInt());
+            humans[countHuman++] = human;
         }
         return humans;
     }
 }
+
